017488759f574c7a19181b4a7c59e7121c12a1ad
Jordan Latimer <jordanjlatimer@gmail.com>
2022-11-23 15:09:56 -0700

    Fix deps and remove comment

--- jest-preset.js
+++ jest-preset.js
@@ -1,8 +1,4 @@
 const { defaults: tsjPreset } = require("ts-jest/presets")
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
 
 module.exports = {
   transform: tsjPreset.transform,
